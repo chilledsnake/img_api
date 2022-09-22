6db2fb392f5b990a514686905669e77f98b452fb
chilledsnake <chilledsnake.pl@gmail.com>
2022-09-22 22:42:22 +0200

    remove import

--- src/api/images/migrations/0002_alter_uploadedimages_options_and_more.py
+++ src/api/images/migrations/0002_alter_uploadedimages_options_and_more.py
@@ -17,8 +17,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="uploadedimages",
             name="image_file",
-            field=models.ImageField(
-                upload_to="images", verbose_name="uploaded image"
-            ),
+            field=models.ImageField(upload_to="images", verbose_name="uploaded image"),
         ),
     ]
