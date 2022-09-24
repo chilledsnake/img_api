5e2b70c106d61fd9b32a6a36f9586b768101596f
chilledsnake <chilledsnake.pl@gmail.com>
2022-09-24 23:00:49 +0200

    add pytest DEFAULT_FILE_STORAGE

--- src/api/images/tests/conftest.py
+++ src/api/images/tests/conftest.py
@@ -7,9 +7,9 @@ from src.api.images.tests.factories import UploadedImagesFactory
 
 
 def pytest_configure():
-    settings.USE_AWS_S3 = False
     settings.MEDIA_URL = "/media/"
     settings.MEDIA_ROOT = os.path.join(settings.BASE_DIR, "media")
+    settings.DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 
 register(UploadedImagesFactory)
